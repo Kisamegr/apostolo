dcb9fa253dfccac4e214c3a685d41ee1c2df347f
Sokratis Papadopoulos <sokpao@gmail.com>
2014-05-14 17:23:20 +0300

    Tries to make some functions

--- tbd_project/tbd_project/dbtproj.cpp
+++ tbd_project/tbd_project/dbtproj.cpp
@@ -2,7 +2,15 @@
 #include <cstdlib>
 #include <cstring>
 #include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
+
+void ReadFile(FILE *infile, block_t block);
+void GenerateFile(FILE *outfile, int nblocks);
+
 
 int main(int argc, char** argv) {
 
@@ -12,9 +20,11 @@ int main(int argc, char** argv) {
 	unsigned int recid = 0;
 	FILE *infile, *outfile;
 
+
 	// generate a file 
 	outfile = fopen("file.bin", "w");
-
+	//GenerateFile(outfile, nblocks);
+	
 	for (int b = 0; b<nblocks; ++b) { // for each block
 
 		block.blockid = b;
@@ -37,32 +47,53 @@ int main(int argc, char** argv) {
 
 	fclose(outfile);
 
-	// open file and print contents
+	
+	//read file
 	infile = fopen("file.bin", "r");
-	int nreserved;
-	while (!feof(infile)) { // while end-of-file has not been reached ... 
+	ReadFile(infile, block);
 
-		fread(&block, 1, sizeof(block_t), infile); // read the next block
-		nreserved = block.nreserved;
-
-		// print block contents
-		for (int i = 0; i<nreserved; ++i) {
-			printf("this is block id: %d, record id: %d, num: %d, str: %s\n",
-				block.blockid, block.entries[i].recid, block.entries[i].num, block.entries[i].str);
-		}
-	}
-
-	fclose(infile);
 	system("pause");
 	return 0;
 }
 
 
+/**
+   infile: the name of the input file
+   field: which field will be used for sorting: 0 is for recid, 1 is for num, 2 is for str and 3 is for both num and str
+   buffer: pointer to memory buffer
+   nmem_blocks: number of blocks in memory
+   outfile: the name of the output file
+   nsorted_segs: number of sorted segments produced (this should be set by you)
+   npasses: number of passes required for sorting (this should be set by you)
+   nios: number of IOs performed (this should be set by you)
 
+*/
 void MergeSort(char *infile, unsigned char field, block_t *buffer, unsigned int nmem_blocks, char *outfile, unsigned int *nsorted_segs, unsigned int *npasses, unsigned int *nios)
 {
 	// FIRST PART
+	/*
+	
+	// open file and print contents
+	FILE *input = fopen(infile, "r");
+	while (!feof(input)) { // while end-of-file has not been reached ... 
 
+		fread(&block, 1, sizeof(block_t), input); // read the next block
+		nreserved = block.nreserved;
+
+		// print block contents
+		for (int i = 0; i<nreserved; ++i) {
+			printf("this is block id: %d, record id: %d, num: %d, str: %s\n",
+			block.blockid, block.entries[i].recid, block.entries[i].num, block.entries[i].str);
+		}
+	}
+
+	fclose(input);
+
+	for (int i = 0; i < 21; i++)
+	{
+
+	}*/
+	//read the input file
 	//Read each block one at a time
 	//When a block is read, put it in the buffer
 	//Then quicksort the records in the block
@@ -84,6 +115,16 @@ void MergeSort(char *infile, unsigned char field, block_t *buffer, unsigned int 
 
 }
 
+/* ----------------------------------------------------------------------------------------------------------------------
+infile: the name of the input file
+field: which field will be used for sorting: 0 is for recid, 1 is for num, 2 is for str and 3 is for both num and str
+buffer: pointer to memory buffer
+nmem_blocks: number of blocks in memory
+outfile: the name of the output file
+nunique: number of unique records in file (this should be set by you)
+nios: number of IOs performed (this should be set by you)
+----------------------------------------------------------------------------------------------------------------------
+*/
 void EliminateDuplicates(char *infile, unsigned char field, block_t *buffer, unsigned int nmem_blocks, char *outfile, unsigned int *nunique, unsigned int *nios)
 {
 	// FIRST PART
@@ -97,6 +138,17 @@ void EliminateDuplicates(char *infile, unsigned char field, block_t *buffer, uns
 
 }
 
+/* ----------------------------------------------------------------------------------------------------------------------
+infile1: the name of the first input file
+infile2: the name of the second input file
+field: which field will be used for the join: 0 is for recid, 1 is for num, 2 is for str and 3 is for both num and str
+buffer: pointer to memory buffer
+nmem_blocks: number of blocks in memory
+outfile: the name of the output file
+nres: number of pairs in output (this should be set by you)
+nios: number of IOs performed (this should be set by you)
+----------------------------------------------------------------------------------------------------------------------
+*/
 void MergeJoin(char *infile1, char *infile2, unsigned char field, block_t *buffer, unsigned int nmem_blocks, char *outfile, unsigned int *nres, unsigned int *nios)
 {
 	// Read the 2 files
@@ -107,16 +159,17 @@ void MergeJoin(char *infile1, char *infile2, unsigned char field, block_t *buffe
 }
 
 
-block_t* GenerateFile(int nblocks)
+void GenerateFile(FILE *outfile, int nblocks)
 {
+	// generate a file 
+	block_t block;
 	record_t record;
-	block_t* block = new block_t[nblocks];
 	unsigned int recid = 0;
 
-	for (int b = 0; b < nblocks; ++b) { // for each block
+	for (int b = 0; b<nblocks; ++b) { // for each block
 
-		block[b].blockid = b;
-		for (int r = 0; r < MAX_RECORDS_PER_BLOCK; ++r) { // for each record
+		block.blockid = b;
+		for (int r = 0; r<MAX_RECORDS_PER_BLOCK; ++r) { // for each record
 
 			// prepare a record
 			record.recid = recid++;
@@ -124,14 +177,16 @@ block_t* GenerateFile(int nblocks)
 			strcpy(record.str, "hello");   // put the same string to all records
 			record.valid = true;
 
-			memcpy(&block[b].entries[r], &record, sizeof(record_t)); // copy record to block
+			memcpy(&block.entries[r], &record, sizeof(record_t)); // copy record to block
 		}
 
-		block[b].nreserved = MAX_RECORDS_PER_BLOCK;
-		block[b].valid = true;
+		block.nreserved = MAX_RECORDS_PER_BLOCK;
+		block.valid = true;
+
+		fwrite(&block, 1, sizeof(block_t), outfile);	// write the block to the file
 	}
 
-	return block;
+	fclose(outfile);
 }
 
 void WriteFile()
@@ -139,8 +194,22 @@ void WriteFile()
 
 }
 
-void ReadFile()
+void ReadFile(FILE *infile, block_t block)
 {
+	// open file and print contents
+	int nreserved;
+	while (!feof(infile)) { // while end-of-file has not been reached ... 
 
+		fread(&block, 1, sizeof(block_t), infile); // read the next block
+		nreserved = block.nreserved;
+
+		// print block contents
+		for (int i = 0; i<nreserved; ++i) {
+			printf("this is block id: %d, record id: %d, num: %d, str: %s\n",
+				block.blockid, block.entries[i].recid, block.entries[i].num, block.entries[i].str);
+		}
+	}
+
+	fclose(infile);
 }
 
